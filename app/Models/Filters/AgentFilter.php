b90a20c5bfa74fc798fece4ef7426b2c8320100a
j <j@gmail.com>
2024-10-18 17:02:24 +0800

    x

--- app/Models/Filters/AgentFilter.php
+++ app/Models/Filters/AgentFilter.php
@@ -2,6 +2,15 @@
 
 namespace App\Models\Filters;
 
+use App\Models\Agent;
+
 class AgentFilter extends BaseModelFilter
 {
+    public function trashed($value): void
+    {
+        if ($value === 'only') {
+            /** @var Agent $this */
+            $this->onlyTrashed();
+        }
+    }
 }
