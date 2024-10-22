834d4f650d069c43ceeb1e42d414d5b20a91b38b
j <j@gmail.com>
2024-10-22 15:28:51 +0800

    x

--- app/Models/Filters/DepositFilter.php
+++ app/Models/Filters/DepositFilter.php
@@ -4,4 +4,13 @@ namespace App\Models\Filters;
 
 class DepositFilter extends BaseModelFilter
 {
+    public function tradeNo($value): void
+    {
+        $this->where('trade_no', $value);
+    }
+
+    public function depositableType($value): void
+    {
+        $this->where('depositable_type', $value);
+    }
 }
