9e946c441cee8b6fa96aa3188f683b2c107a1017
j <j@gmail.com>
2024-10-23 09:42:19 +0800

    x

--- app/Models/Filters/DepositFilter.php
+++ app/Models/Filters/DepositFilter.php
@@ -14,6 +14,11 @@ class DepositFilter extends BaseModelFilter
         $this->where('depositable_type', $value);
     }
 
+    public function depositable($value): void
+    {
+        $this->where('depositable_id', $value);
+    }
+
     public function currency($value): void
     {
         $this->where('currency_id', $value);
