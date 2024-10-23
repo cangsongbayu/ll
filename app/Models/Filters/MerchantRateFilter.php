2cb1063abcce37b4dc310b9fcaa75af382b6cc2f
j <j@gmail.com>
2024-10-23 11:12:49 +0800

    x

--- app/Models/Filters/MerchantRateFilter.php
+++ app/Models/Filters/MerchantRateFilter.php
@@ -2,6 +2,15 @@
 
 namespace App\Models\Filters;
 
+use App\Models\MerchantRate;
+
 class MerchantRateFilter extends BaseModelFilter
 {
+    public function trashed($value): void
+    {
+        if ($value === 'only') {
+            /** @var MerchantRate $this */
+            $this->onlyTrashed();
+        }
+    }
 }
